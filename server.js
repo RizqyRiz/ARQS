d49f8293267cb26c1c77f754a6b2c182380223f9
Danial Rizqy <rizqy7386@gmail.com>
2024-04-14 02:35:17 +0800

    test

--- server.js
+++ server.js
@@ -255,6 +255,6 @@ app.get('/api/arqs/game/prop/:propId', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(3306, () => {
+    console.log('Yo!');
 });
